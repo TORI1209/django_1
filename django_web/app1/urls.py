c3fe7305098254cf06d64323b4432172417958d5
tori <noram.1209.t.r@gmail.com>
2024-08-13 23:32:30 +0900

    2

--- django_web/app1/urls.py
+++ django_web/app1/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from .views import IndexView
+from .views import HelpView
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('help/', HelpView.as_view()),
 ]
--- django_web/app1/views.py
+++ django_web/app1/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
+
+class HelpView(TemplateView):
+    template_name = "help_index.html"
