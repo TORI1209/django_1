8d3af5e8c37541da538cb27afc86b617346bd62b
TORI1209 <noram.1209.t.r@gmail.com>
2024-09-20 22:35:46 +0900

    5 json

--- django_web/app1/views.py
+++ django_web/app1/views.py
@@ -1,6 +1,12 @@
 from typing import Any
 from django.views.generic import TemplateView
 
+#　json の import
+from django.shortcuts import render
+import json
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
+
 
 class IndexView(TemplateView):
     template_name = "index.html"
@@ -10,6 +16,19 @@ class IndexView(TemplateView):
         #名前入力
         ctxt["username"] = ""   
         return ctxt
+    
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        with open("C:/Users/EDO-NET User/Desktop/django_1/django_web/app1/information.json","r") as json_read:
+            json_data = json.load(json_read)
+            ctxt["date"] = json_data["information"][0]["date"] 
+            ctxt["title"] = json_data["information"][0]["title"] 
+            ctxt["context"] = json_data["information"][0]["context"] 
+
+            return ctxt
+            
+    
+
 
 class ProfileView(TemplateView):
     template_name = "profile_index.html"
@@ -29,3 +48,4 @@ class ProfileView(TemplateView):
         ctxt["practice_skills"] = practice_skills
 
         return ctxt
+    
